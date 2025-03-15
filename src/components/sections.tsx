553e0f4f092d776b56cbfacecbe09fbc950a4367
Kevyn Rodrigues <kevyncodes@gmail.com>
2025-03-15 04:05:04 -0300

    Refactor styles: replace SCSS with CSS and update global styles configuration

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "@/styles/globals.scss";
+import "@/styles/globals.css";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
 import javascriptIcon from "@/assets/jsicon.svg";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.scss";
+import "@/styles/globals.css";
 import Head from "next/head";
 import Image from "next/image";
 import imagePerfil from "@/assets/fotoperfil.png";
